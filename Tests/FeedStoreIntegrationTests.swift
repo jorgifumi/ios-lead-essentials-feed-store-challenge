6e41acb74f4ba5abe2224446ef4afdc7a897678c
Jorge Lucena <jorgifumi@gmail.com>
2021-01-17 18:20:07 +0100

    Implement setupEmptyStoreState and undoStoreSideffects functions

--- Tests/FeedStoreIntegrationTests.swift
+++ Tests/FeedStoreIntegrationTests.swift
@@ -80,11 +80,15 @@ class FeedStoreIntegrationTests: XCTestCase {
 	}
 	
 	private func setupEmptyStoreState() {
-		
+		deleteStoreArtifacts()
 	}
 	
 	private func undoStoreSideEffects() {
-		
+		deleteStoreArtifacts()
+	}
+
+	private func deleteStoreArtifacts() {
+		try? FileManager.default.removeItem(at: testSpecificStoreURL())
 	}
 
 	private func testSpecificStoreURL() -> URL {
